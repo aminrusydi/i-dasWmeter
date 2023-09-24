b3640d5cde42bbfa2d1e2536ea82507a37288c6a
amin rusydi <aminrusydii@gmail.com>
2023-09-24 10:01:11 +0700

    Updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,8 @@
 #include <Arduino.h>
-#include <LiquidCrystal_I2C.h>
-
-// #include <Keypad_MC17.h>
-// #include <Keypad.h>
-// #include <ctype.h>
 #include <Wire.h>
 #include <SPI.h>
-
 #include <ArduinoJson.h>
 #include <StreamUtils.h>
-#include <TinyGPSPlus.h>
 
 #include <Adafruit_ADS1X15.h>
 Adafruit_ADS1115 ads;
@@ -20,12 +13,11 @@ unsigned long upIdleTime_now = 0;
 const unsigned long periodValve = 1000;
 unsigned long valveTime_now = 0;
 
-LiquidCrystal_I2C lcd(0x27, 20, 4);
-
 // kondisi Button
 const int relayPower = 32;
 const int pbPower = 27;
 const int pbEmergency = 33;
+int kondisiEmergency;
 int kondisiPower;
 
 // Konfirmasi Data Masuk
@@ -33,7 +25,7 @@ bool confirmTransaction = false;
 bool confirmConfig = false;
 
 // SoftwareSerial Serial1(14, 15);
-TinyGPSPlus gps;
+
 double latitude;
 double longitude;
 
@@ -55,12 +47,27 @@ int literSebelumnya;
 float battVolt;
 const float multiplier = 0.1875F;
 int persenBatt;
+String statBatt;
+bool hold = false;
+const unsigned long periodBatt = 1000;
+unsigned long battTime_now = 0;
+const unsigned long periodUpbatt = 2000;
+unsigned long uptimebat = 0;
+
+const int numReadings = 10;
+int readings[numReadings]; // the readings from the analog input
+int readIndex = 0;         // the index of the current reading
+int total = 0;             // the running total
+int average = 0;
 
 // Kondisi Proses
 bool snyc = 1;
 bool idle = 1;
 bool process = 0;
 int jumlahPesanan = 0;
+int pembagi;
+int setProgress;
+int statMicro;
 
 const String idDevice = "DASWMETER24VL0001";
 const int LED2 = 2;
@@ -90,25 +97,6 @@ int lastSensorState_HF = 0;
 int CableState = 0;
 int DIRState = 0;
 
-// // Keypad Configuration
-// #define I2CADDR 0x20
-
-// const byte ROWS = 5; // four rows
-// const byte COLS = 4; // three columns
-// // Define the keymaps.  The blank spot (lower left) is the space character.
-// char hexaKeys[ROWS][COLS] = {
-//     {'A', 'B', 'C', 'F'},
-//     {'1', '2', '3', 'U'},
-//     {'4', '5', '6', 'D'},
-//     {'7', '8', '9', 'E'},
-//     {'K', '0', 'T', 'N'}};
-
-// byte rowPins[ROWS] = {15, 14, 13, 12, 11}; // connect to the row pinouts of the keypad
-// byte colPins[COLS] = {5, 8, 9, 10};
-// Keypad_MC17 customKeypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS, I2CADDR);
-
-// Fungsi set Flow Cyble
-
 void setCounter(int nilai)
 {
   Serial.println("Set LF");
@@ -131,23 +119,10 @@ void setup()
   EEPROM.begin(512);
   Serial.begin(19200);
   Serial2.begin(19200);
-  Serial1.begin(115200, SERIAL_8N1, 14, 15);
+  Serial1.begin(19200, SERIAL_8N1, 14, 15);
   // Wire.begin();
   // customKeypad.begin();
   ads.begin();
-
-  lcd.init();
-  lcd.backlight();
-  lcd.setCursor(5, 0);
-  lcd.print("i-DasWmeter");
-  lcd.setCursor(0, 2);
-  lcd.print("Stan : ");
-  lcd.setCursor(18, 2);
-  lcd.print("m3");
-  // lcd.setCursor(0, 3);
-  // lcd.print("Batt : ");
-  // lcd.setCursor(19, 3);
-  // lcd.print("%");
 
   // kondisiButton
   pinMode(relayPower, OUTPUT);
@@ -203,15 +178,24 @@ void readGPS()
 {
   if (Serial1.available())
   {
-    gps.encode(Serial1.read());
-  }
-  if (gps.location.isUpdated())
-  {
+    StaticJsonDocument<150> datagps;
+    DeserializationError error = deserializeJson(datagps, Serial1);
+
+    if (error)
+    {
+      Serial.print("deserializeJson() failed: ");
+      Serial.println(error.c_str());
+      return;
+    }
+
+    double latitudee = datagps["lat"];   // "0000"
+    double longitudee = datagps["long"]; // "0000"
+    latitude = latitudee;
+    longitude = longitudee;
+
     StaticJsonDocument<500> doc;
     EepromStream eepromStream(0, 500);
 
-    latitude = gps.location.lat();
-    longitude = gps.location.lng();
     doc["lf"] = CybleCounter_LF;
     doc["lat"] = latitude;
     doc["long"] = longitude;
@@ -232,29 +216,62 @@ void dataUplink()
 
   dataUp["nodeID"] = idDevice;
   dataUp["loc"] = location;
-  dataUp["Batt"] = String(persenBatt) + "%";
+  dataUp["Batt"] = String(statBatt) + "%";
   dataUp["status"] = status;
   dataUp["flow"] = String(flowSend);
   dataUp["logFlow"] = dataLog;
   serializeJson(dataUp, Serial2);
 }
 
+void antarMicroProses()
+{
+  StaticJsonDocument<150> microProses;
+  microProses["req"] = jumlahPesanan;
+  microProses["stan"] = CybleCounter_LF;
+  microProses["count"] = literCounter;
+  microProses["microStat"] = statMicro;
+  // microProses["batt"] = persenBatt;
+  serializeJson(microProses, Serial1);
+}
+
 void prosesPengisian()
 {
 
   digitalWrite(pinValve, HIGH);
+  digitalWrite(pbPower, HIGH);
   Serial.println(CybleCounter_LF);
+  Serial.print("Set : ");
+  Serial.println(setProgress);
 
-  if (literCounter == jumlahPesanan)
+  if (literCounter == setProgress)
+  {
+    flowSend = String(CybleCounter_LF) + "," + String(literCounter);
+    statBatt = String(persenBatt);
+    status = "6"; // Progress Air
+    dataUplink();
+    setProgress = setProgress + pembagi;
+    // delay(500);
+    dataUplink();
+    Serial.println("Masuk Counting");
+    dataUplink();
+    Serial.println("Uplink");
+    dataUplink();
+    Serial.println("Uplink");
+    dataUplink();
+  }
+  if (literCounter = jumlahPesanan || kondisiEmergency == 0 || (persenBatt > 0 && persenBatt < 20) || (literCounter >= jumlahPesanan && persenBatt > 20))
   {
     digitalWrite(pinValve, LOW);
+    status = "3";
     flowSend = String(CybleCounter_LF) + "," + String(literCounter);
     dataLog = String(cybleSebelumnya) + "," + String(literSebelumnya);
-    status = "Transaction Done";
+    statBatt = String(persenBatt);
+    // Transaction Done
     logCyble = CybleCounter_LF;
     logLiter = literCounter;
-
+    Serial.println("Stoping Process");
     dataUplink();
+    Serial.println("Stoping Process");
     delay(5000);
     dataUplink();
     delay(5000);
@@ -262,13 +279,19 @@ void prosesPengisian()
     delay(5000);
     dataUplink();
     delay(15000);
+    statMicro = 0;
+    antarMicroProses();
+    delay(2000);
+    digitalWrite(pbPower, LOW);
     resetFunc();
+
     idle = 1;
   }
 }
 
 void dataDownlink()
 {
+  // if ((Serial2.available() > 0) && idle == true && persenBatt >= 20)
   if ((Serial2.available() > 0) && idle == true)
   {
     StaticJsonDocument<512> list;
@@ -289,41 +312,41 @@ void dataDownlink()
       int txInfo_item_transaction_status = txInfo_item["transaction"]["status"]; // 1, 1
       int txInfo_item_transaction_volume = txInfo_item["transaction"]["volume"]; // 0, 0
 
-      jumlahPesanan = txInfo_item_transaction_volume;
-
       if (String(txInfo_item_forID) == idDevice)
       {
+        jumlahPesanan = txInfo_item_transaction_volume;
         Serial.print("ID :");
         Serial.print(txInfo_item_forID);
-        Serial.print(" status : ");
-        Serial.println(txInfo_item_transaction_status);
+        Serial.print(" volume : ");
+        Serial.println(jumlahPesanan);
+
         if (txInfo_item_transaction_status == 1)
         {
+          if (jumlahPesanan >= 500)
+          {
+            pembagi = jumlahPesanan / 5;
+            setProgress = pembagi;
+            // Serial.println(pembagi);
+          }
           cybleSebelumnya = logCyble;
           literSebelumnya = logLiter;
-          status = "Transaction Received";
-          lcd.clear();
-          lcd.setCursor(2, 0);
-          lcd.print("PROSES PENGISIAN");
-          lcd.setCursor(0, 1);
-          lcd.print("Req  : ");
-          lcd.setCursor(14, 1);
-          lcd.print((float(jumlahPesanan) / 1000), 1);
-          lcd.setCursor(17, 1);
-          lcd.print(" m3");
-          lcd.setCursor(0, 2);
-          lcd.print("Stan : ");
-          lcd.setCursor(17, 2);
-          lcd.print(" m3");
-          lcd.setCursor(0, 3);
-          lcd.print("Count: ");
-          lcd.setCursor(14, 3);
-          lcd.print("0.0 ");
-          lcd.print("m3");
-
-          dataUplink();
+          status = "2"; // Transaction Received
           idle = 0;
-          // delay(2000);
+          dataUplink();
+          delay(500);
+          dataUplink();
+          statMicro = 1;
+          antarMicroProses();
+          delay(1000);
+        }
+        else if (txInfo_item_transaction_status == 2)
+        {
+          antarMicroProses();
+          cybleSebelumnya = logCyble;
+          literSebelumnya = logLiter;
+          Serial.println(jumlahPesanan);
+          status = "2"; // Transaction Received
+          dataUplink();
         }
       }
     }
@@ -332,32 +355,56 @@ void dataDownlink()
 
 void bacaBattrey()
 {
-  int16_t adc0, adc1, adc2, adc3;
-
-  adc0 = ads.readADC_SingleEnded(0);
-  adc1 = ads.readADC_SingleEnded(1);
-  adc2 = ads.readADC_SingleEnded(2);
-  adc3 = ads.readADC_SingleEnded(3);
-
-  battVolt = (6.5078 * (adc0 * multiplier / 1000)) + 0.0321;
-  persenBatt = (battVolt / 26) * 100;
-
-  if (persenBatt > 100)
+  if (millis() >= battTime_now + periodBatt)
   {
-    persenBatt = 100;
-  }
-  if (persenBatt < 0)
-  {
-    persenBatt = 0;
-  }
+    battTime_now += periodBatt;
+    int16_t adc0, adc1, adc2, adc3;
 
-  // Serial.print("Batt: ");
-  // Serial.println(persenBatt);
+    adc0 = ads.readADC_SingleEnded(0);
+    adc1 = ads.readADC_SingleEnded(1);
+    adc2 = ads.readADC_SingleEnded(2);
+    adc3 = ads.readADC_SingleEnded(3);
+
+    battVolt = (5.7676 * (adc0 * multiplier / 1000)) - 0.0403;
+    persenBatt = (50 * (battVolt - 24));
+
+    // battVolt = (6.5078 * (adc0 * multiplier / 1000)) + 0.0321;
+    // persenBatt = (25 * (battVolt - 22));
+    if (persenBatt > 100)
+    {
+      persenBatt = 100;
+    }
+    if (persenBatt < 0)
+    {
+      persenBatt = 0;
+    }
+    statBatt = String(persenBatt);
+    Serial.print("Batt: ");
+    Serial.println(persenBatt);
+  }
 }
 
 void loop()
 {
+
+  // StaticJsonDocument<500> doc;
+  // EepromStream eepromStream(0, 500);
+
+  // doc["lf"] = 1221;
+  // doc["lat"] = latitude;
+  // doc["long"] = longitude;
+  // doc["lfB"] = logCyble;
+  // doc["lB"] = logLiter;
+  // serializeJson(doc, eepromStream);
+
+  // eepromStream.flush();
+  // Serial.print("Idle :");
+  // Serial.println(idle);
+
   SensorState_LF = digitalRead(LF_State);
+  kondisiEmergency = digitalRead(pbEmergency);
+  bacaBattrey();
+  Serial.println(pembagi);
 
   if (SensorState_LF != lastSensorState_LF)
   {
@@ -376,34 +423,11 @@ void loop()
       serializeJson(doc, eepromStream);
       eepromStream.flush();
 
-      // Serial.println("data simpan");
       if (idle == 0)
       {
         literCounter = literCounter + 100;
-        if (((float(CybleCounter_LF) / 10), 1) < 1000 && ((float(CybleCounter_LF) / 10), 1) > 99)
-        {
-
-          lcd.setCursor(12, 2);
-          lcd.print((float(CybleCounter_LF) / 10), 1);
-        }
-
-        if (((float(CybleCounter_LF) / 10), 1) < 100)
-        {
-
-          lcd.setCursor(13, 2);
-          lcd.print((float(CybleCounter_LF) / 10), 1);
-        }
-
-        if ((float(literCounter) / 1000) < 10)
-        {
-          lcd.setCursor(14, 3);
-          lcd.print(float(literCounter) / 1000);
-        }
-        if ((float(literCounter) / 1000) < 100 && (float(literCounter) / 1000) > 9)
-        {
-          lcd.setCursor(13, 3);
-          lcd.print(float(literCounter) / 1000);
-        }
+        statMicro = 1;
+        antarMicroProses();
       }
     }
     lastSensorState_LF = SensorState_LF;
@@ -411,62 +435,55 @@ void loop()
 
   if (idle == 1)
   {
-    bacaBattrey();
     readGPS();
+    digitalWrite(pbPower, LOW);
+    // Serial.print("Batt :");
+    Serial.println(persenBatt);
     dataDownlink();
-    Serial.println(CybleCounter_LF);
     literCounter = 0;
-
-    if (((float(CybleCounter_LF) / 10), 1) < 1000 && ((float(CybleCounter_LF) / 10), 1) > 99)
-    {
-
-      lcd.setCursor(12, 2);
-      lcd.print((float(CybleCounter_LF) / 10), 1);
-    }
-
-    if (((float(CybleCounter_LF) / 10), 1) < 100)
-    {
-
-      lcd.setCursor(13, 2);
-      lcd.print((float(CybleCounter_LF) / 10), 1);
-    }
-
-    // if (persenBatt < 10)
-    // {
-    //   lcd.setCursor(15, 3);
-    //   lcd.print("");
-    //   lcd.setCursor(16, 3);
-    //   lcd.print("");
-    //   lcd.setCursor(17, 3);
-    //   lcd.print(persenBatt);
-    // }
-    // if (persenBatt < 100 && persenBatt > 9)
-    // {
-    //   lcd.setCursor(15, 3);
-    //   lcd.print("");
-    //   lcd.setCursor(16, 3);
-    //   lcd.print(persenBatt);
-    // }
-    // if (persenBatt > 99)
-    // {
-    //   lcd.setCursor(15, 3);
-    //   lcd.print(persenBatt);
-    // }
+    setProgress = 0;
 
     if (millis() >= upIdleTime_now + periodUp_idle)
     {
+      antarMicroProses();
       upIdleTime_now += periodUp_idle;
-
       flowSend = "0,0";
       dataLog = String(logCyble) + "," + String(logLiter);
-      Serial.println("Kondisi idle");
-      status = "Ok";
+      status = "1"; // Siap Digunakan
       dataUplink();
+      Serial.println(logLiter);
     }
   }
 
   if (idle == 0)
   {
+    // Serial.print("Batt :");
+    Serial.println(persenBatt);
+    if (persenBatt == 0)
+    {
+      // btrai dicabut (batrai drop)
+      hold = true;
+      statBatt = "disable";
+      if (millis() >= uptimebat + periodUpbatt)
+      {
+        uptimebat += periodUpbatt;
+        dataUplink();
+        Serial.println(hold);
+        dataUplink();
+        Serial.println(hold);
+        dataUplink();
+      }
+    }
+    if (persenBatt > 20 && hold == true)
+    {
+      statBatt = String(persenBatt);
+      dataUplink();
+      Serial.println(hold);
+      dataUplink();
+      Serial.println(hold);
+      dataUplink();
+      hold = false;
+    }
     prosesPengisian();
   }
   else
